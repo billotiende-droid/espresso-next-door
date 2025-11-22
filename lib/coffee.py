2c91a7fdf20ee9a6c146268e6822e8358c4800b9
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-23 00:16:33 +0300

    code-reconciliation

--- lib/coffee.py
+++ lib/coffee.py
@@ -6,4 +6,17 @@ class Coffee:
     def brew(self):
         return self._brew    
     
-   
+    @brew.setter
+    def brew(self, brew):
+        if not isinstance(brew,str):
+            raise TypeError("Brew must be a string")
+        if len(brew) < 3 :
+            raise ValueError("Brew must be at least 3 characters")
+        self._brew = brew
+
+
+  
+
+
+capucciono = Coffee("Cappuccino")
+print(capucciono.brew)  # Output: Cappuccino
