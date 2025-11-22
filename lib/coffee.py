5c7d5a92576baddd2c1f52da8f11bc467ffe161d
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-22 23:37:53 +0300

    brew-constructor-prop-initiliaze

---
+++ lib/coffee.py
@@ -0,0 +1,10 @@
+class Coffee:
+    def __init__(self,brew):
+        self.brew = brew
+
+    @property
+    def brew(self):
+        return self._brew    
+    
+    @brew.setter
+    def brew(self, brew):
