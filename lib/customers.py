f0c2696bd4b29df4cc2aa1d31896d0fae7947504
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-22 13:23:14 +0300

    email-property-setter-as-truth

--- lib/customers.py
+++ lib/customers.py
@@ -14,6 +14,17 @@ class Customers:
         if len(name) < 1 or len(name) > 15:
             raise ValueError("Name must be between 1 and 15 characters")
         self.name = name
+
+    def email(self):
+        return self.email
+
+    @email.setter
+    def email(self, email):
+        if not isinstance(email,str):
+            raise TypeError("Email must be a string")
+        if "@" not in email or "." not in email:
+            raise ValueError("Email must be a valid email address")
+        self.email = email    
     
 
         
