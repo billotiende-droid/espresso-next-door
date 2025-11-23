d5db279017c53cfddd259c26878c00e0d2beb0bd
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-23 12:59:12 +0300

    order-coffee-relationship-methods

--- lib/customers.py
+++ lib/customers.py
@@ -1,3 +1,5 @@
+from orders import all_orders
+
 class Customers:
     def __init__(self,name, email):
         self.name = name
@@ -14,7 +16,7 @@ class Customers:
         if len(name) < 1 or len(name) > 15:
             raise ValueError("Name must be between 1 and 15 characters")
         self._name = name
-
+    @property
     def email(self):
         return self._email
 
@@ -25,7 +27,14 @@ class Customers:
         if "@" not in email or "." not in email:
             raise ValueError("Email must be a valid email address")
         self._email = email    
-        
+
+    def orders(self):
+        customer_orders = [order for order in all_orders if order.customer == self]
+        return customer_orders    
+    
+    def coffees(self):
+        unique_coffees = [order.brew for order in self.orders()]
+        return list(set(unique_coffees))
     
 
         
