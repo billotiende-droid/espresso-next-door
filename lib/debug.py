8ce32d6ff49cfa04fe5a5a9744091d4408f822b0
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-24 08:59:43 +0300

    customer-order-queries-test-file

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,29 @@
 from customers import Customers
 from coffee import Coffee
-from orders import Orders
-c1 = Customers("Zayn","zayn@hotmail.com")
-c2= Customers("Agnes","agnes@gmail.com")
-brew = Coffee("Espresso")
-brew2 = Coffee("Latte")
-order = Orders(c1, brew, 3.5)
-order2 = Orders(c2, brew2, 4.0) 
+from orders import Order
+
+# Example usage:
+zayn = Customers("Zayn","zayn@gmail.com")
+agnes = Customers("Agnes","agnes@hotmail.com")
 
 
-print([order.coffee.brew for order in c1.orders()])  
-print(c.brew for c in c1.coffees())
+espresso = Coffee("Espresso")
+latte = Coffee("Latte")
+
+Orderr1 = Order(zayn, espresso, 3.5)
+Orderr2 = Order(agnes, latte, 4.0)
+Orderr3 = Order(zayn, latte, 4.5)
+
+
+# customer queries
+print([order.brew.brew for order in zayn.orders()])  # Output: ['Espresso', 'Latte']
+print([brew.brew for brew in zayn.coffees()])    # Output: ['Espresso', 'Latte']
+
+# coffee queries
+print([coffee.name for coffee in latte.customers()])  # Output: ['Agnes', 'Zayn']
+print([coffee.name for coffee in espresso.customers()])  # Output: ['Zayn']
+
+# access order details
+print(Orderr1.customer.name)  # Output: Zayn
+print(Orderr1.brew.brew)      # Output: Latte
+print(Orderr1.price)          # Output: 3.5
